b37ccfe4642a3abcc14b4393e49c463740944021
Silent Malachite <84108752+SilentMalachite@users.noreply.github.com>
2026-02-24 06:31:09 +0900

    chore: バージョンを 0.2.19 に更新 (OTPリリース)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AlchemIiif.MixProject do
   def project do
     [
       app: :alchem_iiif,
-      version: "0.2.7",
+      version: "0.2.19",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
